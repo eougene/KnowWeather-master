55d90cd47bcdf42537c80c11a2b917ad63fa9603
eougene <you@example.com>
2017-02-20 17:00:04 +0800

    第二次提交

---
+++ app/src/main/java/com/silencedut/knowweather/SplashActivity.java
@@ -0,0 +1,38 @@
+package com.silencedut.knowweather;
+
+import android.content.Intent;
+import android.widget.FrameLayout;
+
+import com.silencedut.knowweather.common.BaseActivity;
+
+import butterknife.BindView;
+
+/**
+ * Created by SilenceDut on 2016/11/25 .
+ */
+
+public class SplashActivity extends BaseActivity {
+    @BindView(R.id.splash_fl)
+    FrameLayout mSplashFl;
+
+    @Override
+    public int getContentViewId() {
+        return R.layout.activity_splash;
+    }
+
+    @Override
+    public void initViews() {
+
+        mSplashFl.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                overridePendingTransition(0, 0);
+                finish();
+            }
+        }, 2000);
+
+    }
+
+}
